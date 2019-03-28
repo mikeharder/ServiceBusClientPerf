f703b46b768a9cab0a851502777b88e032afe95d
Mike Harder <mharder@microsoft.com>
2019-03-28 16:40:16 -0700

    Fix max calc

--- js/app.ts
+++ js/app.ts
@@ -69,7 +69,7 @@ async function WriteResults(messages: number): Promise<void> {
     let currentElapsed = elapsed - lastElapsed;
     lastElapsed = elapsed;
 
-    if (currentMessages > maxMessages) {
+    if ((currentMessages / currentElapsed) > (maxMessages / maxElapsed) {
       maxMessages = currentMessages;
       maxElapsed = currentElapsed;
     }
